c8a7b975cf49eb64f95a05797c90db62aa3aaa8a
Joe <yu.xue.work@outlook.com>
2023-06-29 14:01:24 +0800

    feat: rc drop

--- src/third.rs
+++ src/third.rs
@@ -17,29 +17,40 @@ impl<T> List<T> {
     }
 
     pub fn prepend(&self, elem: T) -> List<T> {
-        List { head: Some(Rc::new(Node { 
+        List { head: Some(Rc::new(Node {
             elem: elem,
             next: self.head.clone(),
-        })) }
+        }))}
     }
 
     pub fn tail(&self) -> List<T> {
-        List { head: self.head.as_ref().and_then(|node| {node.next.clone()}) }
-    } 
+        List { head: self.head.as_ref().and_then(|node| node.next.clone()) }
+    }
 
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
     }
 }
 
 pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
-}
-
-impl<T> List<T> {
-    pub fn iter(&self) -> Iter<'_, T> {
-        Iter { next: self.head.as_deref() }
-    }
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
